4a4ffbba3df54b351fb5cf1e3701b49365f179e0
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-03-24 14:55:34 -0300

    Header done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+import RiotLogo from './assets/riot games.png';
+import ValorantLogo from './assets/logo.png';
+import UserProfile from './assets/user_icon.png';
+import ArrowDown from './assets/Polygon.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="header__menu">
+          <div className="header__icons">
+            <img src={RiotLogo} alt="Riot Games" className="icons__riot" />
+            <span className="vertical_bar"></span>
+            <img src={ValorantLogo} alt="Valorant" className="icons__valorant" />
+          </div>
+          <nav>
+            <ul>
+              <li><a href="#Game">game</a></li>
+              <li><a href="#Champions">champions</a></li>
+              <li className="dropdown__menu">
+                news
+                <img src={ArrowDown} alt="arrow down" />
+                <div className="menu__container"></div>
+              </li>
+              <li><a href="#PatchNotes">patch notes</a></li>
+              <li className="dropdown__menu">
+                discover
+                <img src={ArrowDown} alt="arrow down" />
+                <div className="menu__container"></div>
+              </li>
+              <li><a href="#Esports">esports</a></li>
+              <li><a href="#Universe">universe</a></li>
+              <li><a href="#Shop">shop</a></li>
+              <li><a href="#Support">support</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div className="header__user">
+          <img src={UserProfile} alt="user prolife" />
+          <span className="user__name">unepickid</span>
+          <img src={ArrowDown} alt="arrow down" />
+        </div>
       </header>
     </div>
   );
